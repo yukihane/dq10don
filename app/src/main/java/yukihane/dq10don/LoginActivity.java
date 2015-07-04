8c663ec88275d735bf321af0ae84c10f20bdb677
yukihane <yukihane.feather@gmail.com>
2015-07-04 20:49:15 +0900

    トークンアプリ起動処理実装

--- app/src/main/java/yukihane/dq10don/LoginActivity.java
+++ app/src/main/java/yukihane/dq10don/LoginActivity.java
@@ -1,15 +1,19 @@
 package yukihane.dq10don;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
-import android.util.Log;
-import android.view.ViewGroup.LayoutParams;
-import android.support.v7.app.ActionBarActivity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
+import android.widget.Button;
+
+import java.util.List;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -23,12 +27,33 @@ public class LoginActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // トークンアプリがインストールされていなければ(intentを受け取れるアプリが存在しないなら)　ボタンをdisableに
+        // 参考: https://developer.android.com/training/basics/intents/sending.html
+        Intent otpIntent = createOtpIntent();
+
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(otpIntent, 0);
+        boolean isIntentSafe = activities.size() > 0;
+
+        Button button = (Button) findViewById(R.id.launchOtpButton);
+        button.setEnabled(isIntentSafe);
+
         webView = (WebView) findViewById(R.id.loginWebView);
         webView.setVerticalScrollBarEnabled(false);
         webView.setHorizontalScrollBarEnabled(false);
         webView.setWebViewClient(new LoginView());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(OAUTH_URL + "oauthauth?client_id=happy&redirect_uri=https%3A%2F%2Fhappy.dqx.jp%2Fcapi%2Flogin%2Fsecurelogin%2F&response_type=code&yl=1");
+    }
+
+    private Intent createOtpIntent() {
+        Uri uri = Uri.parse("sqextoken://appviewotp");
+        return new Intent(Intent.ACTION_VIEW, uri);
+    }
+
+    public void launchOtpApp(View view) {
+        Intent intent = createOtpIntent();
+        startActivity(intent);
     }
 
     private class LoginView extends WebViewClient {
@@ -43,7 +68,7 @@ public class LoginActivity extends ActionBarActivity {
         public void onReceivedError(WebView view, int errorCode,
                                     String description, String failingUrl) {
             Log.d("methodcalled", "onReceivedError");
-            super.onReceivedError(view,errorCode,description,failingUrl);
+            super.onReceivedError(view, errorCode, description, failingUrl);
         }
 
         @Override
