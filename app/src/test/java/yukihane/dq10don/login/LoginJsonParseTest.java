f95e190842eff41a0b1d5482fc66629db3e385a3
yukihane <yukihane.feather@gmail.com>
2015-07-06 13:08:02 +0900

    テストケース実装

--- app/src/test/java/yukihane/dq10don/login/LoginJsonParseTest.java
+++ app/src/test/java/yukihane/dq10don/login/LoginJsonParseTest.java
@@ -1,6 +1,7 @@
 package yukihane.dq10don.login;
 
 import static org.junit.Assert.*;
+
 import android.annotation.TargetApi;
 import android.os.Build;
 
@@ -13,6 +14,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 import yukihane.dq10don.login.LoginAccountDto;
 import yukihane.dq10don.login.LoginJsonParse;
@@ -36,6 +38,9 @@ public class LoginJsonParseTest {
         }
     }
 
+    /**
+     * JSONが想定通りPOJOに変換されるかのテスト
+     */
     @Test
     public void testProcessHTML() throws Exception {
         LoginJsonParse parse = new LoginJsonParse();
@@ -43,5 +48,25 @@ public class LoginJsonParseTest {
         LoginAccountDto result = parse.getResult();
 
         assertEquals(1, result.getAccountType());
+        assertEquals("dummy_cisuserid", result.getCisuserid());
+        assertEquals(0, result.getResultCode());
+        assertEquals("sessionid_dummy", result.getSessionId());
+
+        List<CharacterDto> cList = result.getCharacterList();
+        assertEquals(3, cList.size());
+
+        CharacterDto c1 = cList.get(0);
+        assertEquals(c1.getCharacterName(), "キャラ名1");
+        assertTrue(c1.getIconUrl().startsWith("http"));
+        assertEquals(c1.getJob(), "レンジャー");
+
+        CharacterDto c2 = cList.get(1);
+        assertEquals(3, c2.getJobId());
+        assertEquals(56, c2.getLv());
+
+        CharacterDto c3 = cList.get(2);
+        assertEquals("ZZ777-777", c3.getSmileUniqueNo());
+        assertEquals(6666666L, c3.getWebPcNo());
+
     }
 }
