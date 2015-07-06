7d2ff6d19a07dd6bf390361f48b74678e12344f6
yukihane <yukihane.feather@gmail.com>
2015-07-06 16:16:11 +0900

    クラス責務変更に伴ってテストコードの場所も変更

---
+++ app/src/test/java/yukihane/dq10don/login/LoginAccountDtoTest.java
@@ -0,0 +1,104 @@
+package yukihane.dq10don.login;
+
+import static org.junit.Assert.*;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+/**
+ * Created by yuki on 15/07/05.
+ */
+public class LoginAccountDtoTest {
+
+    private static String jsonSuccess;
+    private static String jsonFail = "{\"resultCode\":999}";
+    private static String jsonError = "<html><head></head><body>login error</body></html>";
+    private ObjectMapper mapper;
+
+    /**
+     * ファイルの最初の1行を読み込みます.
+     *
+     * @param filename 読み込むファイル名
+     * @return 読み込んだ内容
+     */
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private static String readLine(String filename) throws URISyntaxException, IOException {
+
+        URI file = LoginAccountDtoTest.class.getClassLoader().getResource(filename).toURI();
+
+        try (FileReader reader = new FileReader(new File(file))) {
+            BufferedReader br = new BufferedReader(reader);
+            return br.readLine();
+        }
+    }
+
+    @BeforeClass
+    public static void beforeClass() throws IOException, URISyntaxException {
+        jsonSuccess = readLine("login_success.json");
+    }
+
+    @Before
+    public void before() {
+        mapper = new ObjectMapper();
+    }
+
+    /**
+     * JSONが想定通りPOJOに変換されるかのテスト
+     */
+    @Test
+    public void testSuccess() throws Exception {
+        LoginAccountDto result = mapper.readValue(jsonSuccess, LoginAccountDto.class);
+
+        assertEquals(1, result.getAccountType());
+        assertEquals("dummy_cisuserid", result.getCisuserid());
+        assertEquals(0, result.getResultCode());
+        assertEquals("sessionid_dummy", result.getSessionId());
+
+        List<CharacterDto> cList = result.getCharacterList();
+        assertEquals(3, cList.size());
+
+        CharacterDto c1 = cList.get(0);
+        assertEquals(c1.getCharacterName(), "キャラ名1");
+        assertTrue(c1.getIconUrl().startsWith("http"));
+        assertEquals(c1.getJob(), "レンジャー");
+
+        CharacterDto c2 = cList.get(1);
+        assertEquals(3, c2.getJobId());
+        assertEquals(56, c2.getLv());
+
+        CharacterDto c3 = cList.get(2);
+        assertEquals("ZZ777-777", c3.getSmileUniqueNo());
+        assertEquals(6666666L, c3.getWebPcNo());
+    }
+
+    /**
+     * ログイン失敗した場合のjsonが帰ってきた場合のテストケース.
+     * 実際にどのようなものが帰ってくるかわからないので, resultCodeが非0であると想定しています.
+     */
+    @Test
+    public void testFail() throws IOException {
+        LoginAccountDto result = mapper.readValue(jsonFail, LoginAccountDto.class);
+        assertEquals(999, result.getResultCode());
+    }
+
+    @Test(expected = IOException.class)
+    public void testLoginError() throws IOException {
+        LoginAccountDto result = mapper.readValue(jsonError, LoginAccountDto.class);
+    }
+}
