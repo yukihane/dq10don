92aa58856e46d62f4787527d69df6dca9045e4b1
yukihane <yukihane.feather@gmail.com>
2015-07-06 13:29:56 +0900

    コメント追加

--- app/src/main/java/yukihane/dq10don/login/LoginJsonParse.java
+++ app/src/main/java/yukihane/dq10don/login/LoginJsonParse.java
@@ -11,6 +11,9 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * ログイン処理を行った結果はJSON形式で帰ってきますが, そのJSONをJavaオブジェクトに変換するクラスです.
+ */
 public class LoginJsonParse {
 
     private static final Pattern PATTERN = Pattern.compile("\\{.+\\}");
@@ -18,6 +21,11 @@ public class LoginJsonParse {
     private final Logger logger = LoggerFactory.getLogger(LoginJsonParse.class);
     private LoginAccountDto result;
 
+    /**
+     * 変換処理を実行します.
+     *
+     * @param html 受け取った結果. 呼び出し側の都合上, jsonが書かれたhtml形式です.
+     */
     @JavascriptInterface
     public void processHTML(String html) {
         try {
@@ -32,6 +40,11 @@ public class LoginJsonParse {
         }
     }
 
+    /**
+     * 直前に実行した変換処理結果を取得します.
+     *
+     * @return 変換結果. 処理を行っていない場合や変換処理でエラーが発生した場合にはnull.
+     */
     public LoginAccountDto getResult() {
         return result;
     }
