44279d1956d232c092a458dd904f9da29a4b9778
yukihane <yukihane.feather@gmail.com>
2015-07-08 08:50:07 +0900

    characterselectテスト

---
+++ app/src/test/java/yukihane/dq10don/communication/dto/CharaSelectDtoTest.java
@@ -0,0 +1,55 @@
+package yukihane.dq10don.communication.dto;
+
+import static org.junit.Assert.*;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by yuki on 15/07/08.
+ */
+public class CharaSelectDtoTest {
+
+    private static File successFile;
+    private static File failFile;
+    private ObjectMapper mapper;
+
+    @BeforeClass
+    public static void beforeClass() throws URISyntaxException {
+        successFile = new File(CharaSelectDtoTest.class.getClassLoader()
+                .getResource("dummy_response/characterselect_success.json").toURI());
+        failFile = new File(CharaSelectDtoTest.class.getClassLoader()
+                .getResource("dummy_response/characterselect_fail.json").toURI());
+    }
+
+    @Before
+    public void setUp() {
+        mapper = new ObjectMapper();
+    }
+
+    @Test
+    public void testCreateSuccess() throws IOException {
+        CharaSelectDto res = mapper.readValue(successFile, CharaSelectDto.class);
+        assertEquals(0, res.getResultCode());
+        assertEquals("hogeHoge", res.getEncWebPcNo());
+    }
+
+    @Test
+    public void testCreateFail() throws IOException {
+        CharaSelectDto res = mapper.readValue(failFile, CharaSelectDto.class);
+        assertEquals(100, res.getResultCode());
+        assertNull(res.getEncWebPcNo());
+    }
+}
