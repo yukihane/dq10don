0ae3d1d58baa4d774b1040293a4a8c4f1a84703e
yukihane <yukihane.feather@gmail.com>
2015-07-07 17:48:09 +0900

    通信を行うサービスとそのファクトリクラスの雛形

---
+++ app/src/main/java/yukihane/dq10don/communication/HappyService.java
@@ -0,0 +1,7 @@
+package yukihane.dq10don.communication;
+
+/**
+ * Created by yuki on 15/07/07.
+ */
+public interface HappyService {
+}
---
+++ app/src/main/java/yukihane/dq10don/communication/HappyServiceFactory.java
@@ -0,0 +1,9 @@
+package yukihane.dq10don.communication;
+
+/**
+ * Created by yuki on 15/07/07.
+ */
+public class HappyServiceFactory {
+    private HappyServiceFactory() {
+    }
+}
