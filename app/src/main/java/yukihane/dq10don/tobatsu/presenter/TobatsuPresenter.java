2259c21ec9731a5394d250db4af2c209e51ddcd8
yukihane <yukihane.feather@gmail.com>
2015-07-19 16:36:41 +0900

    起動時に情報をロードする

--- app/src/main/java/yukihane/dq10don/tobatsu/presenter/TobatsuPresenter.java
+++ app/src/main/java/yukihane/dq10don/tobatsu/presenter/TobatsuPresenter.java
@@ -5,18 +5,24 @@ import com.j256.ormlite.android.apptools.OpenHelperManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.SQLException;
+
+import retrofit.RetrofitError;
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 import yukihane.dq10don.account.TobatsuItem;
+import yukihane.dq10don.account.TobatsuList;
 import yukihane.dq10don.communication.HappyService;
 import yukihane.dq10don.communication.HappyServiceFactory;
 import yukihane.dq10don.communication.dto.tobatsu.TobatsuDataList;
 import yukihane.dq10don.communication.dto.tobatsu.TobatsuDto;
 import yukihane.dq10don.db.DbHelper;
 import yukihane.dq10don.db.DbHelperFactory;
+import yukihane.dq10don.tobatsu.model.TobatsuService;
+import yukihane.dq10don.tobatsu.model.TobatsuServiceFactory;
 
 /**
  * Created by yuki on 15/07/15.
@@ -41,6 +47,57 @@ public class TobatsuPresenter {
 
     public void onViewCreated() {
         view.setHeader(character.getSqexid(), character.getSmileUniqNo());
+
+        Observable<TobatsuList> observable = Observable.create(new Observable.OnSubscribe<TobatsuList>() {
+            @Override
+            public void call(Subscriber<? super TobatsuList> subscriber) {
+                subscriber.onStart();
+
+                TobatsuService service = new TobatsuServiceFactory().getService(dbHelper);
+                try {
+                    TobatsuList tl = service.getTobatsuList(character.getWebPcNo());
+                    subscriber.onNext(tl);
+                } catch (SQLException e) {
+                    LOGGER.error("tobatsu list query error", e);
+                    subscriber.onError(e);
+                } catch (RetrofitError e) {
+                    LOGGER.error("retrofit error: url: {}, message: {}, type: {}, body: {}",
+                            e.getUrl(), e.getMessage(), e.getSuccessType(), e.getBody());
+
+                    LOGGER.error("", e);
+                    subscriber.onError(e);
+                }
+                subscriber.onCompleted();
+            }
+        });
+
+        observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+
+        view.bindToList(observable);
+
+        observable.subscribe(new Observer<TobatsuList>() {
+            private TobatsuList tobatsuList;
+
+            @Override
+            public void onCompleted() {
+                LOGGER.debug("onCompleted");
+                if (tobatsuList != null) {
+                    view.tobatsuListUpdate(tobatsuList);
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                LOGGER.error("error occured", e);
+            }
+
+            @Override
+            public void onNext(TobatsuList tobatsuList) {
+                LOGGER.debug("onNext");
+                this.tobatsuList = tobatsuList;
+            }
+        });
+
     }
 
     public void onUpdateClick() {
