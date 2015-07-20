a0a5d5c9719674d1271621a3d809538bbd0dd119
yukihane <yukihane.feather@gmail.com>
2015-07-20 19:52:13 +0900

    アラームとアラームで起動するサービス

---
+++ app/src/main/java/yukihane/dq10don/background/AlarmReceiver.java
@@ -0,0 +1,43 @@
+package yukihane.dq10don.background;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Calendar;
+
+
+/**
+ * https://developer.android.com/training/scheduling/alarms.html を参考に作成.
+ */
+public class AlarmReceiver extends WakefulBroadcastReceiver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AlarmReceiver.class);
+
+    public static void setAlarm(Context context) {
+        LOGGER.info("setAlarm called");
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 10);
+        alarmMgr.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), alarmIntent);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        LOGGER.info("onReceive called");
+        ComponentName component = new ComponentName(context, RoundService.class);
+        intent.setComponent(component);
+
+        startWakefulService(context, intent);
+    }
+}
