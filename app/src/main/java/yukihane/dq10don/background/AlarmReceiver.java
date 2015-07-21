f55bcaa854620618316a98716a5f52f9bbac0d15
yukihane <yukihane.feather@gmail.com>
2015-07-21 11:38:31 +0900

    アラームキャンセル処理

--- app/src/main/java/yukihane/dq10don/background/AlarmReceiver.java
+++ app/src/main/java/yukihane/dq10don/background/AlarmReceiver.java
@@ -11,8 +11,6 @@ import android.support.v4.content.WakefulBroadcastReceiver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Calendar;
-
 import yukihane.dq10don.Application;
 
 
@@ -29,15 +27,46 @@ public class AlarmReceiver extends WakefulBroadcastReceiver {
     public static void setAlarm(Application application, long timeInMillis) {
         LOGGER.info("setAlarm called");
         Context context = application.getApplicationContext();
+
+        PendingIntent alarmIntent = getPendingIntent(application);
+
         AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(context, AlarmReceiver.class);
-
-        PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
-
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.SECOND, 30);
-        alarmMgr.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), alarmIntent);
+        alarmMgr.set(AlarmManager.RTC_WAKEUP, timeInMillis, alarmIntent);
         LOGGER.info("Alarm set");
+
+        // 起動時にアラームをセットできるようにする
+        ComponentName receiver = new ComponentName(context, AutoSetReceiver.class);
+        PackageManager pm = context.getPackageManager();
+
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+
+    }
+
+    public static void cancelAlarm(Application application) {
+        LOGGER.info("cancelAlarm called");
+        Context context = application.getApplicationContext();
+
+        PendingIntent alarmIntent = getPendingIntent(application);
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmMgr.cancel(alarmIntent);
+        LOGGER.info("Alarm cancelled");
+
+
+        // 起動時に自動的にアラームをセットしない
+        ComponentName receiver = new ComponentName(context, AutoSetReceiver.class);
+        PackageManager pm = context.getPackageManager();
+
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
+    private static PendingIntent getPendingIntent(Application application) {
+        Context context = application.getApplicationContext();
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 
     @Override
