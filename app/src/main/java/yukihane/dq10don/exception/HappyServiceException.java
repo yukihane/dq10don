14bd2a827842c537b350e8d52e363a8317b968d3
yukihane <yukihane.feather@gmail.com>
2015-07-23 09:09:34 +0900

    RetroFitError を捕捉例外でラップする

--- app/src/main/java/yukihane/dq10don/exception/HappyServiceException.java
+++ app/src/main/java/yukihane/dq10don/exception/HappyServiceException.java
@@ -1,15 +1,39 @@
 package yukihane.dq10don.exception;
 
+import retrofit.RetrofitError;
+
 /**
- * Created by yuki on 15/07/22.
+ * HappyServiceにリクエストし失敗に終わった場合に送出される例外です.
+ * {@link #getType()} によって例外の種類を取得できます.
+ * {@link yukihane.dq10don.exception.HappyServiceException.Type#SERVERSIDE} の場合,
+ * {@link #getResultCode()} でエラー原因が判別できます.
+ * {@link yukihane.dq10don.exception.HappyServiceException.Type#HTTP} の場合,
+ * {@link #getCause()} によってRetrofitErrorが取得できますのでそれで判別します.
  */
 public class HappyServiceException extends Exception {
 
+    private final Type type;
+
     private final int resultCode;
+
+    private final RetrofitError error;
 
     public HappyServiceException(int resultCode) {
         super();
+        this.type = Type.SERVERSIDE;
         this.resultCode = resultCode;
+        this.error = null;
+    }
+
+    public HappyServiceException(String detailMessage, RetrofitError throwable) {
+        super(detailMessage, throwable);
+        this.type = Type.HTTP;
+        this.resultCode = -1;
+        this.error = throwable;
+    }
+
+    public Type getType() {
+        return type;
     }
 
     public int getResultCode() {
@@ -17,7 +41,32 @@ public class HappyServiceException extends Exception {
     }
 
     @Override
+    public RetrofitError getCause() {
+        return error;
+    }
+
+    @Override
     public String getMessage() {
-        return "Happy Service Error Return CODE: " + resultCode;
+        switch (type) {
+            case SERVERSIDE:
+                return "Happy Service Error Return CODE: " + resultCode;
+            case HTTP:
+            default:
+                return super.getMessage();
+        }
+    }
+
+    /**
+     * 例外の種類です.
+     */
+    public enum Type {
+        /**
+         * サーバーへは正常に到達しているが, サーバーがエラーを返した.
+         */
+        SERVERSIDE,
+        /**
+         * ネットワークの問題(サーバーに到達していない, あるいは到達したがstatus codeが200以外).
+         */
+        HTTP,
     }
 }
