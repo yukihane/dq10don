914e36ec786af626123a91845266c4526a8326da
yukihane <yukihane.feather@gmail.com>
2015-07-24 13:50:16 +0900

    定数を使用して書き換え

--- app/src/main/java/yukihane/dq10don/ViewUtils.java
+++ app/src/main/java/yukihane/dq10don/ViewUtils.java
@@ -5,6 +5,12 @@ import android.content.Context;
 import retrofit.RetrofitError;
 import yukihane.dq10don.exception.HappyServiceException;
 
+import static yukihane.dq10don.communication.HappyServiceResultCode.HOUSEBAZAAR_UNSET;
+import static yukihane.dq10don.communication.HappyServiceResultCode.INGAME;
+import static yukihane.dq10don.communication.HappyServiceResultCode.NORMAL;
+import static yukihane.dq10don.communication.HappyServiceResultCode.TOBATSUQUEST_NEVER_ACCEPTED;
+import static yukihane.dq10don.communication.HappyServiceResultCode.TOBATSU_SLOW_SERVICE;
+
 /**
  * Created by yuki on 15/07/22.
  */
@@ -21,16 +27,16 @@ public class ViewUtils {
             case SERVERSIDE:
                 int resultCode = ex.getResultCode();
                 switch (resultCode) {
-                    case 0:
+                    case NORMAL:
                         // 0 は正常コード
                         return "";
-                    case 106:
+                    case INGAME:
                         return context.getString(R.string.happy_106);
-                    case 12009:
+                    case HOUSEBAZAAR_UNSET:
                         return context.getString(R.string.happy_12009);
-                    case 22001:
+                    case TOBATSU_SLOW_SERVICE:
                         return context.getString(R.string.happy_22001);
-                    case 22002:
+                    case TOBATSUQUEST_NEVER_ACCEPTED:
                         // 討伐が受けられるようになるクエストをこなしていない
                         return context.getString(R.string.happy_22002);
                     default:
