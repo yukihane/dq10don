9b8facc51615d01f4c711615cce5b06b3e00cf58
yukihane <yukihane.feather@gmail.com>
2015-07-26 09:27:36 +0900

    ダウンロード先ディレクトリ表示処理

--- app/src/main/java/yukihane/dq10don/debug/view/DebugActivity.java
+++ app/src/main/java/yukihane/dq10don/debug/view/DebugActivity.java
@@ -1,7 +1,11 @@
 package yukihane.dq10don.debug.view;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+import java.io.File;
 
 import yukihane.dq10don.R;
 
@@ -11,5 +15,10 @@ public class DebugActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug);
+
+        File toDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        TextView downloadDirView = (TextView) findViewById(R.id.downloadDirView);
+        downloadDirView.setText(toDir.getAbsolutePath());
+
     }
 }
