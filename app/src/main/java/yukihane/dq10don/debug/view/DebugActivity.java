ca294b99d773b2a10b898d1d6214f19ea0888443
yukihane <yukihane.feather@gmail.com>
2015-07-26 09:19:51 +0900

    メニューは不要なので削除

--- app/src/main/java/yukihane/dq10don/debug/view/DebugActivity.java
+++ app/src/main/java/yukihane/dq10don/debug/view/DebugActivity.java
@@ -1,9 +1,7 @@
 package yukihane.dq10don.debug.view;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 import yukihane.dq10don.R;
 
@@ -13,27 +11,5 @@ public class DebugActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_debug, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
