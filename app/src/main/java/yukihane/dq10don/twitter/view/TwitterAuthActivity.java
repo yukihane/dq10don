50342f45754e21c46c3bb525b37491d891837259
yukihane <yukihane.feather@gmail.com>
2015-07-26 14:56:09 +0900

    Twitter認証Activityの自動生成コード

---
+++ app/src/main/java/yukihane/dq10don/twitter/view/TwitterAuthActivity.java
@@ -0,0 +1,18 @@
+package yukihane.dq10don.twitter.view;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import yukihane.dq10don.R;
+
+public class TwitterAuthActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter_auth);
+    }
+
+}
