9ef3c411468e221c83aebcee0689c6f0bc2452ce
yukihane <yukihane.feather@gmail.com>
2015-07-27 20:14:37 +0900

    不要コード削除

--- app/src/main/java/yukihane/dq10don/settings/view/SettingsActivity.java
+++ app/src/main/java/yukihane/dq10don/settings/view/SettingsActivity.java
@@ -34,13 +34,7 @@ import java.util.List;
  * API Guide</a> for more information on developing a Settings UI.
  */
 public class SettingsActivity extends PreferenceActivity {
-    /**
-     * Determines whether to always show the simplified settings UI, where
-     * settings are presented in a single list. When false, settings are shown
-     * as a master/detail two-pane view on tablets. When true, a single pane is
-     * shown on tablets.
-     */
-    private static final boolean ALWAYS_SIMPLE_PREFS = false;
+
     /**
      * A preference value change listener that updates the preference's summary
      * to reflect its new value.
