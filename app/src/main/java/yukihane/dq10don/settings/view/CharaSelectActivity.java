b13a6fcecddb3d7d88926bca18293ccaf92afeaa
yukihane <yukihane.feather@gmail.com>
2015-07-27 22:18:16 +0900

    討伐tweet対象キャラクタ選択Activity自動生成

---
+++ app/src/main/java/yukihane/dq10don/settings/view/CharaSelectActivity.java
@@ -0,0 +1,18 @@
+package yukihane.dq10don.settings.view;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import yukihane.dq10don.R;
+
+public class CharaSelectActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chara_select);
+    }
+
+}
