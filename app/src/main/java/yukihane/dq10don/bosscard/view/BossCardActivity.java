86390b0bb41916e7ce40c4beff3939f5fda1be34
yukihane <yukihane.feather@gmail.com>
2015-07-29 19:53:41 +0900

    自動生成Activity

---
+++ app/src/main/java/yukihane/dq10don/bosscard/view/BossCardActivity.java
@@ -0,0 +1,50 @@
+package yukihane.dq10don.bosscard.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import yukihane.dq10don.R;
+import yukihane.dq10don.bosscard.presenter.BossCardPresenter;
+import yukihane.dq10don.debug.view.DebugActivity;
+import yukihane.dq10don.settings.view.SettingsActivity;
+import yukihane.dq10don.sqexid.view.SqexidActivity;
+
+public class BossCardActivity extends AppCompatActivity implements BossCardPresenter.View {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boss_card);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_sqexid) {
+            Intent intent = new Intent(this, SqexidActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_debug) {
+            Intent intent = new Intent(this, DebugActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
