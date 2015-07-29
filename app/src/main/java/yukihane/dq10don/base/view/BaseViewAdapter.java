c1931f425f6c2e41b7aff9804b7a4c5035ff8e52
yukihane <yukihane.feather@gmail.com>
2015-07-30 05:22:55 +0900

    getView pull up

--- app/src/main/java/yukihane/dq10don/base/view/BaseViewAdapter.java
+++ app/src/main/java/yukihane/dq10don/base/view/BaseViewAdapter.java
@@ -3,6 +3,8 @@ package yukihane.dq10don.base.view;
 import android.database.DataSetObservable;
 import android.database.DataSetObserver;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListAdapter;
 import android.widget.SpinnerAdapter;
 
@@ -37,6 +39,33 @@ public abstract class BaseViewAdapter<T> implements ListAdapter, SpinnerAdapter 
         types.clear();
         displayTargets.clear();
     }
+
+    /**
+     * Get a View that displays the data at the specified position in the data set. You can either
+     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+     * parent View (GridView, ListView...) will apply default layout parameters unless you use
+     * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
+     * to specify a root view and to prevent attachment to the root.
+     *
+     * @param position    The position of the item within the adapter's data set of the item whose view
+     *                    we want.
+     * @param convertView The old view to reuse, if possible. Note: You should check that this view
+     *                    is non-null and of an appropriate type before using. If it is not possible to convert
+     *                    this view to display the correct data, this method can create a new view.
+     *                    Heterogeneous lists can specify their number of view types, so that this View is
+     *                    always of the right type (see {@link #getViewTypeCount()} and
+     *                    {@link #getItemViewType(int)}).
+     * @param parent      The parent that this view will eventually be attached to
+     * @return A View corresponding to the data at the specified position.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return getView(position, convertView, parent,
+                inflater, types.get(position), displayTargets.get(position));
+    }
+
+    protected abstract View getView(int position, View convertView, ViewGroup parent,
+                                    LayoutInflater inflater, T type, Object displayTarget);
 
     /**
      * Register an observer that is called when changes happen to the data used by this adapter.
--- app/src/main/java/yukihane/dq10don/tobatsu/view/TobatsuViewAdapter.java
+++ app/src/main/java/yukihane/dq10don/tobatsu/view/TobatsuViewAdapter.java
@@ -1,7 +1,5 @@
 package yukihane.dq10don.tobatsu.view;
 
-import android.database.DataSetObservable;
-import android.database.DataSetObserver;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +8,6 @@ import android.widget.AdapterView;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.List;
 
 import yukihane.dq10don.R;
 import yukihane.dq10don.account.TobatsuItem;
@@ -47,30 +43,12 @@ public class TobatsuViewAdapter extends BaseViewAdapter<Class<?>> {
         }
     }
 
-    /**
-     * Get a View that displays the data at the specified position in the data set. You can either
-     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
-     * parent View (GridView, ListView...) will apply default layout parameters unless you use
-     * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
-     * to specify a root view and to prevent attachment to the root.
-     *
-     * @param position    The position of the item within the adapter's data set of the item whose view
-     *                    we want.
-     * @param convertView The old view to reuse, if possible. Note: You should check that this view
-     *                    is non-null and of an appropriate type before using. If it is not possible to convert
-     *                    this view to display the correct data, this method can create a new view.
-     *                    Heterogeneous lists can specify their number of view types, so that this View is
-     *                    always of the right type (see {@link #getViewTypeCount()} and
-     *                    {@link #getItemViewType(int)}).
-     * @param parent      The parent that this view will eventually be attached to
-     * @return A View corresponding to the data at the specified position.
-     */
-
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    protected View getView(int position, View convertView, ViewGroup parent,
+                           LayoutInflater inflater, Class<?> type, Object displayTarget) {
+
         final View v;
 
-        Class<?> type = types.get(position);
         if (String.class == type) {
             final TextView textView;
             if (convertView == null) {
@@ -81,7 +59,7 @@ public class TobatsuViewAdapter extends BaseViewAdapter<Class<?>> {
                 v = convertView;
                 textView = (TextView) v.getTag();
             }
-            String obj = (String) displayTargets.get(position);
+            String obj = (String) displayTarget;
             textView.setText(obj);
 
             v.setBackgroundColor(Color.GRAY);
@@ -107,7 +85,7 @@ public class TobatsuViewAdapter extends BaseViewAdapter<Class<?>> {
                 condition = holder.condition;
                 point = holder.point;
             }
-            TobatsuItem obj = (TobatsuItem) displayTargets.get(position);
+            TobatsuItem obj = (TobatsuItem) displayTarget;
             targetName.setText(obj.getMonsterName());
             condition.setText(obj.getArea() + ", " + obj.getCount());
 
@@ -125,6 +103,7 @@ public class TobatsuViewAdapter extends BaseViewAdapter<Class<?>> {
 
         return v;
     }
+
 
     /**
      * Get the type of View that will be created by {@link #getView} for the specified item.
