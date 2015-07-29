b2bea1eb21410d33ee663816d90b61e2332dbf22
yukihane <yukihane.feather@gmail.com>
2015-07-30 05:27:09 +0900

    細かなメソッドをpull up

--- app/src/main/java/yukihane/dq10don/tobatsu/view/TobatsuViewAdapter.java
+++ app/src/main/java/yukihane/dq10don/tobatsu/view/TobatsuViewAdapter.java
@@ -134,65 +134,6 @@ public class TobatsuViewAdapter extends BaseViewAdapter<Class<?>> {
         return DISPLAYABLE_TYPES.length;
     }
 
-    /**
-     * @return true if this adapter doesn't contain any data.  This is used to determine
-     * whether the empty view should be displayed.  A typical implementation will return
-     * getCount() == 0 but since getCount() includes the headers and footers, specialized
-     * adapters might want a different behavior.
-     */
-    @Override
-    public boolean isEmpty() {
-        return displayTargets.isEmpty();
-    }
-
-    /**
-     * Indicates whether all the items in this adapter are enabled. If the
-     * value returned by this method changes over time, there is no guarantee
-     * it will take effect.  If true, it means all items are selectable and
-     * clickable (there is no separator.)
-     *
-     * @return True if all items are enabled, false otherwise.
-     * @see #isEnabled(int)
-     */
-    @Override
-    public boolean areAllItemsEnabled() {
-        return false;
-    }
-
-    /**
-     * Returns true if the item at the specified position is not a separator.
-     * (A separator is a non-selectable, non-clickable item).
-     * <p>
-     * The result is unspecified if position is invalid. An {@link ArrayIndexOutOfBoundsException}
-     * should be thrown in that case for fast failure.
-     *
-     * @param position Index of the item
-     * @return True if the item is not a separator
-     * @see #areAllItemsEnabled()
-     */
-    @Override
-    public boolean isEnabled(int position) {
-        return false;
-    }
-
-    /**
-     * <p>Get a {@link View} that displays in the drop down popup
-     * the data at the specified position in the data set.</p>
-     *
-     * @param position    index of the item whose view we want.
-     * @param convertView the old view to reuse, if possible. Note: You should
-     *                    check that this view is non-null and of an appropriate type before
-     *                    using. If it is not possible to convert this view to display the
-     *                    correct data, this method can create a new view.
-     * @param parent      the parent that this view will eventually be attached to
-     * @return a {@link View} corresponding to the data at the
-     * specified position.
-     */
-    @Override
-    public View getDropDownView(int position, View convertView, ViewGroup parent) {
-        return null;
-    }
-
     private static class TobatsuItemViewHolder {
         public TextView targetName;
         public TextView condition;
