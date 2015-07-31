b1d9033ba1b1310bd7e8a3387957270ccfefd041
yukihane <yukihane.feather@gmail.com>
2015-07-31 21:51:13 +0900

    viewAdapter実装

--- app/src/main/java/yukihane/dq10don/bosscard/view/BossCardListViewAdapter.java
+++ app/src/main/java/yukihane/dq10don/bosscard/view/BossCardListViewAdapter.java
@@ -1,9 +1,17 @@
 package yukihane.dq10don.bosscard.view;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.text.NumberFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import yukihane.dq10don.R;
+import yukihane.dq10don.account.StoredItem;
 import yukihane.dq10don.base.view.BaseViewAdapter;
 
 /**
@@ -11,18 +19,58 @@ import yukihane.dq10don.base.view.BaseViewAdapter;
  */
 public class BossCardListViewAdapter extends BaseViewAdapter<Void> {
 
+    private static final Pattern PATTERN = Pattern.compile("あと(\\d+)(.+)有効");
+
+
     public BossCardListViewAdapter(LayoutInflater inflater) {
         super(inflater);
     }
 
     @Override
     protected void validate(Void type, Object displayTarget) {
-        throw new UnsupportedOperationException();
+        // 1種類しか表示しないので特に処理することはない
     }
 
     @Override
     protected View getView(int position, View convertView, ViewGroup parent, LayoutInflater inflater, Void type, Object displayTarget) {
-        throw new UnsupportedOperationException();
+
+        final View v;
+
+        final TextView cardName;
+        final TextView leftTime;
+        if (convertView == null) {
+            v = inflater.inflate(R.layout.list_boss_card, null);
+            cardName = (TextView) v.findViewById(R.id.bossCardNameView);
+            leftTime = (TextView) v.findViewById(R.id.bossCardTimeView);
+
+            final ViewHolder holder = new ViewHolder();
+            holder.cardName = cardName;
+            holder.leftTime = leftTime;
+            v.setTag(holder);
+        } else {
+            v = convertView;
+            ViewHolder holder = (ViewHolder) v.getTag();
+            cardName = holder.cardName;
+            leftTime = holder.leftTime;
+        }
+        StoredItem obj = (StoredItem) displayTarget;
+        cardName.setText(obj.getItemName());
+
+        Matcher m = PATTERN.matcher(obj.getVariousStr());
+        m.matches();
+        int timeNum = Integer.parseInt(m.group(1));
+
+        NumberFormat nf = NumberFormat.getNumberInstance();
+        leftTime.setText(nf.format(timeNum) + m.group(2));
+
+        if (position % 2 == 0) {
+            v.setBackgroundColor(Color.LTGRAY);
+        } else {
+            v.setBackgroundColor(Color.WHITE);
+        }
+
+
+        return v;
     }
 
     @Override
@@ -33,5 +81,10 @@ public class BossCardListViewAdapter extends BaseViewAdapter<Void> {
     @Override
     public int getViewTypeCount() {
         return 1;
+    }
+
+    private static class ViewHolder {
+        public TextView cardName;
+        public TextView leftTime;
     }
 }
