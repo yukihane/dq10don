63d67c00c0a8209a89d54f8fae00539e282054e3
yukihane <yukihane.feather@gmail.com>
2015-07-31 15:56:50 +0900

    activity関連のパーツを準備

---
+++ app/src/main/java/yukihane/dq10don/bosscard/presenter/BossCardPresenter.java
@@ -0,0 +1,10 @@
+package yukihane.dq10don.bosscard.presenter;
+
+/**
+ * Created by yuki on 15/07/31.
+ */
+public class BossCardPresenter {
+
+    public interface View {
+    }
+}
--- app/src/main/java/yukihane/dq10don/bosscard/view/BossCardActivity.java
+++ app/src/main/java/yukihane/dq10don/bosscard/view/BossCardActivity.java
@@ -12,7 +12,7 @@ import yukihane.dq10don.debug.view.DebugActivity;
 import yukihane.dq10don.settings.view.SettingsActivity;
 import yukihane.dq10don.sqexid.view.SqexidActivity;
 
-public class BossCardActivity extends AppCompatActivity {
+public class BossCardActivity extends AppCompatActivity implements BossCardPresenter.View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
