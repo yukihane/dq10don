b6bad6cf131dfc1d12273b27824d05eaa24eb492
yukihane <yukihane.feather@gmail.com>
2015-07-31 18:12:12 +0900

    BossCardアクティビティ実装

--- app/src/main/java/yukihane/dq10don/bosscard/presenter/BossCardPresenter.java
+++ app/src/main/java/yukihane/dq10don/bosscard/presenter/BossCardPresenter.java
@@ -1,10 +1,71 @@
 package yukihane.dq10don.bosscard.presenter;
 
-/**
- * Created by yuki on 15/07/31.
- */
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import yukihane.dq10don.account.Account;
+import yukihane.dq10don.db.AccountDao;
+import yukihane.dq10don.db.DbHelper;
+import yukihane.dq10don.db.DbHelperFactory;
+
 public class BossCardPresenter {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BossCardPresenter.class);
+
+    private View view;
+    private DbHelper dbHelper;
+
+    public BossCardPresenter(View view, DbHelperFactory dbHelperFactory) {
+        this.view = view;
+        this.dbHelper = dbHelperFactory.create();
+    }
+
+    /**
+     * @param firstBoot (Activityが復元されたのではなく)起動された場合にtrue.
+     */
+    public void onCreate(boolean firstBoot) {
+        try {
+            showWelcomeDialogIfNeeded(firstBoot);
+        } catch (SQLException e) {
+            LOGGER.error("initial process error", e);
+        }
+    }
+
+    public void onStart() {
+        try {
+            AccountDao dao = AccountDao.create(dbHelper);
+            List<Account> accounts = dao.queryAll();
+            view.setAccounts(accounts);
+        } catch (SQLException e) {
+            LOGGER.error("account load error", e);
+        }
+    }
+
+    public void onDestroy() {
+        OpenHelperManager.releaseHelper();
+        view = null;
+    }
+
+    private void showWelcomeDialogIfNeeded(boolean boot) throws SQLException {
+        if (!boot) {
+            return;
+        }
+
+        AccountDao dao = AccountDao.create(dbHelper);
+        if (!dao.exists()) {
+            view.showWelcomeDialog();
+        }
+    }
+
     public interface View {
+
+        void showWelcomeDialog();
+
+        void setAccounts(List<Account> accounts);
     }
 }
--- app/src/main/java/yukihane/dq10don/bosscard/view/BossCardActivity.java
+++ app/src/main/java/yukihane/dq10don/bosscard/view/BossCardActivity.java
@@ -1,23 +1,65 @@
 package yukihane.dq10don.bosscard.view;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.List;
+
 import yukihane.dq10don.R;
+import yukihane.dq10don.account.Account;
+import yukihane.dq10don.background.RoundService;
 import yukihane.dq10don.bosscard.presenter.BossCardPresenter;
+import yukihane.dq10don.db.DbHelperFactory;
 import yukihane.dq10don.debug.view.DebugActivity;
 import yukihane.dq10don.settings.view.SettingsActivity;
 import yukihane.dq10don.sqexid.view.SqexidActivity;
+import yukihane.dq10don.tobatsu.view.WelcomeDialog;
 
 public class BossCardActivity extends AppCompatActivity implements BossCardPresenter.View {
+
+    private BossCardPresenter presenter;
+    private BossCardListPagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_boss_card);
+        setContentView(R.layout.activity_main);
+
+        presenter = new BossCardPresenter(this, new DbHelperFactory(this));
+
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.baseListPager);
+        viewPager.setOffscreenPageLimit(3);
+        FragmentManager fm = getSupportFragmentManager();
+
+        pagerAdapter = new BossCardListPagerAdapter(fm);
+
+        viewPager.setAdapter(pagerAdapter);
+
+
+        presenter.onCreate(savedInstanceState == null);
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.cancel(RoundService.TOBATSU_NOTIFICATION_ID);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        presenter.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        presenter.onDestroy();
     }
 
     @Override
@@ -47,4 +89,15 @@ public class BossCardActivity extends AppCompatActivity implements BossCardPrese
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void setAccounts(List<Account> accounts) {
+        pagerAdapter.setAccounts(accounts);
+    }
+
+    @Override
+    public void showWelcomeDialog() {
+        new WelcomeDialog().show(getSupportFragmentManager(), "WelcomeDialog");
+    }
+
 }
