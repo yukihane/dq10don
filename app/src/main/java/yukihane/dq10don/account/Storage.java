95de1c78c581990271f1ae26564cff5d3a5bcad0
yukihane <yukihane.feather@gmail.com>
2015-07-31 10:57:22 +0900

    dtoからエンティティを生成するメソッド(static factory)

--- app/src/main/java/yukihane/dq10don/account/Storage.java
+++ app/src/main/java/yukihane/dq10don/account/Storage.java
@@ -5,6 +5,10 @@ import com.j256.ormlite.table.DatabaseTable;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+import yukihane.dq10don.communication.dto.profile.StorageListValueList;
+
 /**
  * 持ち物({@link StoredItem})をいれる容器.
  * (strageId, storageIndex)が自然キーになります.
@@ -16,19 +20,37 @@ public class Storage {
      * 種別(装備欄、とか、倉庫、とか)の識別子.
      */
     @DatabaseField(canBeNull = false)
+    @Getter
     private int storageId;
 
     /**
      * 同一種別の中の個体識別番号.
      */
     @DatabaseField(canBeNull = false)
+    @Getter
     private int storageIndex;
 
-    @DatabaseField(foreign = true, canBeNull = false)
-    private Character character;
-
     @DatabaseField
+    @Getter
     private String storageName;
 
+    @DatabaseField(foreign = true, canBeNull = false)
+    @Getter
+    @Setter
+    private Character character;
+
     private List<StoredItem> storedItems;
+
+    public Storage() {
+    }
+
+    public Storage(int storageId, int storageIndex, String storageName) {
+        this.storageId = storageId;
+        this.storageIndex = storageIndex;
+        this.storageName = storageName;
+    }
+
+    public static Storage from(StorageListValueList dto) {
+        return new Storage(dto.getStorageId(), dto.getStorageIndex(), dto.getStorageName());
+    }
 }
--- app/src/main/java/yukihane/dq10don/account/StoredItem.java
+++ app/src/main/java/yukihane/dq10don/account/StoredItem.java
@@ -4,6 +4,9 @@ package yukihane.dq10don.account;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
+import lombok.Getter;
+import yukihane.dq10don.communication.dto.profile.ItemBasicListValueList;
+
 /**
  * 持ち物.
  */
@@ -14,6 +17,7 @@ public class StoredItem {
     private String itemUniqueNo;
 
     @DatabaseField(canBeNull = false)
+    @Getter
     private String itemName;
 
     @DatabaseField
@@ -23,5 +27,22 @@ public class StoredItem {
      * カードの場合には残り時間が「あとx時間有効」という文面で入っています.
      */
     @DatabaseField
+    @Getter
     private String variousStr;
+
+    public StoredItem() {
+
+    }
+
+    public StoredItem(String itemUniqueNo, String itemName, String webItemId, String variousStr) {
+        this.itemUniqueNo = itemUniqueNo;
+        this.itemName = itemName;
+        this.webItemId = webItemId;
+        this.variousStr = variousStr;
+    }
+
+
+    public static StoredItem from(ItemBasicListValueList dto) {
+        return new StoredItem(dto.getItemUniqueNo(), dto.getItemName(), dto.getWebItemId(), dto.getVariousStr());
+    }
 }
