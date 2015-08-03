94be109561da7e97ec3430bfb0658569986cffa8
yukihane <yukihane.feather@gmail.com>
2015-08-04 05:45:26 +0900

    自動巡回設定変更に伴うアラームのON/OFF

--- app/src/main/java/yukihane/dq10don/settings/view/TobatsuSettingActivity.java
+++ app/src/main/java/yukihane/dq10don/settings/view/TobatsuSettingActivity.java
@@ -1,12 +1,28 @@
 package yukihane.dq10don.settings.view;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
+
 import yukihane.dq10don.R;
+import yukihane.dq10don.background.TobatsuAlarm;
+import yukihane.dq10don.db.BgServiceDao;
+import yukihane.dq10don.db.DbHelper;
+import yukihane.dq10don.db.DbHelperFactory;
 
 public class TobatsuSettingActivity extends PreferenceActivity {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TobatsuSettingActivity.class);
+
+    private DbHelper dbHelper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,5 +34,36 @@ public class TobatsuSettingActivity extends PreferenceActivity {
         prefMgr.setSharedPreferencesMode(MODE_PRIVATE);
 
         addPreferencesFromResource(R.xml.pref_tobatsu);
+
+        dbHelper = new DbHelperFactory(this).create();
+
+        Preference autoPilot = findPreference(TobatsuPrefUtils.AUTO_PILOT);
+        autoPilot.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                boolean autoPilot = (boolean) newValue;
+                if (autoPilot) {
+                    try {
+                        BgServiceDao dao = BgServiceDao.create(dbHelper);
+                        TobatsuAlarm.setAlarm(getApplicationContext(), dao.get().getNextAlarmTime());
+                    } catch (SQLException e) {
+                        LOGGER.error("db error", e);
+                    }
+                } else {
+                    TobatsuAlarm.cancelAlarm(getApplicationContext());
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (dbHelper != null) {
+            OpenHelperManager.releaseHelper();
+            dbHelper = null;
+        }
     }
 }
