0df0bbdc9e2842a9dd8d51b74005d0f698d64305
yukihane <yukihane.feather@gmail.com>
2015-08-04 04:35:02 +0900

    設定アクティビティ処理実装

--- app/src/main/java/yukihane/dq10don/settings/view/SettingsActivity.java
+++ app/src/main/java/yukihane/dq10don/settings/view/SettingsActivity.java
@@ -1,17 +1,30 @@
 package yukihane.dq10don.settings.view;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import yukihane.dq10don.R;
 
-public class SettingsActivity extends ActionBarActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        TextView bossCard = (TextView) findViewById(R.id.settingBossCardView);
+        bossCard.setOnClickListener((v) -> {
+            Intent intent = new Intent(SettingsActivity.this, BossCardSettingActivity.class);
+            startActivity(intent);
+        });
+
+        TextView twitter = (TextView) findViewById(R.id.settingTwitterView);
+        twitter.setOnClickListener((v) -> {
+            Intent intent = new Intent(SettingsActivity.this, TwitterSettingActivity.class);
+            startActivity(intent);
+        });
     }
 }
