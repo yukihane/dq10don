5476ffc813b0e0867eed966d16f03be1c317751a
yukihane <yukihane.feather@gmail.com>
2015-08-04 05:03:12 +0900

    設定Activityから各種設定起動

--- app/src/main/java/yukihane/dq10don/settings/view/SettingsActivity.java
+++ app/src/main/java/yukihane/dq10don/settings/view/SettingsActivity.java
@@ -3,7 +3,7 @@ package yukihane.dq10don.settings.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
 
 import yukihane.dq10don.R;
 
@@ -14,14 +14,19 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        View tobatsu = findViewById(R.id.settingTobatsuView);
+        tobatsu.setOnClickListener((v) -> {
+            Intent intent = new Intent(SettingsActivity.this, TobatsuSettingActivity.class);
+            startActivity(intent);
+        });
 
-        TextView bossCard = (TextView) findViewById(R.id.settingBossCardView);
+        View bossCard = findViewById(R.id.settingBossCardView);
         bossCard.setOnClickListener((v) -> {
             Intent intent = new Intent(SettingsActivity.this, BossCardSettingActivity.class);
             startActivity(intent);
         });
 
-        TextView twitter = (TextView) findViewById(R.id.settingTwitterView);
+        View twitter = findViewById(R.id.settingTwitterView);
         twitter.setOnClickListener((v) -> {
             Intent intent = new Intent(SettingsActivity.this, TwitterSettingActivity.class);
             startActivity(intent);
