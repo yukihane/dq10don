5a6983b3e6a6b6ea09ff2335c7e6377777b47e02
yukihane <yukihane.feather@gmail.com>
2015-08-04 04:16:52 +0900

    設定アクティビティ新規作成

---
+++ app/src/main/java/yukihane/dq10don/settings/view/SettingsActivity.java
@@ -0,0 +1,17 @@
+package yukihane.dq10don.settings.view;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import yukihane.dq10don.R;
+
+public class SettingsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
