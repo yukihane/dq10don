11581d472bbdc0e12e8bd9891441651de7574d8a
yukihane <yukihane.feather@gmail.com>
2015-08-04 05:40:40 +0900

    自動巡回設定値取得

--- app/src/main/java/yukihane/dq10don/settings/view/TobatsuPrefUtils.java
+++ app/src/main/java/yukihane/dq10don/settings/view/TobatsuPrefUtils.java
@@ -1,6 +1,7 @@
 package yukihane.dq10don.settings.view;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 /**
  * Created by yuki on 15/08/04.
@@ -9,9 +10,20 @@ public final class TobatsuPrefUtils {
 
     public static final String PREF_NAME = "tobatsu";
 
+    public static final String AUTO_PILOT = "autopilot";
+
     private final Context context;
 
     public TobatsuPrefUtils(Context context) {
         this.context = context;
+    }
+
+    public boolean isAutoPilotEnabled() {
+        return getPrefs().getBoolean(AUTO_PILOT, true);
+    }
+
+
+    private SharedPreferences getPrefs() {
+        return context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
     }
 }
