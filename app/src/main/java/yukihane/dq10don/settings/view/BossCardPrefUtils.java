64550a52ce5c0a36724236b4286ec8a9b147b4c7
yukihane <yukihane.feather@gmail.com>
2015-08-04 04:32:13 +0900

    ボスカード設定アクティビティ新規作成

---
+++ app/src/main/java/yukihane/dq10don/settings/view/BossCardPrefUtils.java
@@ -0,0 +1,17 @@
+package yukihane.dq10don.settings.view;
+
+import android.content.Context;
+
+/**
+ * Created by yuki on 15/08/04.
+ */
+public final class BossCardPrefUtils {
+
+    public static final String PREF_NAME = "bosscard";
+
+    private final Context context;
+
+    public BossCardPrefUtils(Context context) {
+        this.context = context;
+    }
+}
