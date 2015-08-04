21c2109cf577fb4d3800641dab9a67afb2d74547
yukihane <yukihane.feather@gmail.com>
2015-08-04 14:19:23 +0900

    ボスカード有効期限通知機能

--- app/src/main/java/yukihane/dq10don/background/BossCardRoundService.java
+++ app/src/main/java/yukihane/dq10don/background/BossCardRoundService.java
@@ -1,8 +1,11 @@
 package yukihane.dq10don.background;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 
@@ -11,10 +14,15 @@ import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 
+import yukihane.dq10don.R;
+import yukihane.dq10don.Utils;
 import yukihane.dq10don.bosscard.model.BossCardListService;
 import yukihane.dq10don.bosscard.model.BossCardListServiceFactory;
+import yukihane.dq10don.bosscard.view.BossCardActivity;
 import yukihane.dq10don.db.DbHelper;
 import yukihane.dq10don.db.DbHelperFactory;
+import yukihane.dq10don.db.StorageDao;
+import yukihane.dq10don.settings.view.BossCardPrefUtils;
 
 /**
  * Created by yuki on 15/08/03.
@@ -43,6 +51,8 @@ public class BossCardRoundService extends IntentService {
             dbHelper = new DbHelperFactory(this).create();
             BossCardListService service = new BossCardListServiceFactory().getService(dbHelper);
             service.getTobatsuListsFromServer();
+            notifyIfNeed(dbHelper);
+
         } catch (SQLException e) {
             LOGGER.error("bosscardroundservice error", e);
         } finally {
@@ -50,5 +60,42 @@ public class BossCardRoundService extends IntentService {
                 OpenHelperManager.releaseHelper();
             }
         }
+    }
+
+    private void notifyIfNeed(DbHelper dbHelper) throws SQLException {
+        BossCardPrefUtils prefUtils = new BossCardPrefUtils(this);
+
+        if (!prefUtils.isNotification()) {
+            return;
+        }
+        int leftTimeLimit = prefUtils.getLeftTimeLimit();
+        int leftMinitesLimit = leftTimeLimit * 24 * 60;
+        StorageDao dao = StorageDao.create(dbHelper);
+        boolean exists = dao.existsLimitLessThan(leftMinitesLimit);
+        if (exists) {
+            sendNotification();
+        }
+    }
+
+    private void sendNotification() {
+        String title = getString(R.string.title_activity_boss_card);
+        String msg = getString(R.string.text_exits_limit_items);
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, BossCardActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_notify)
+                        .setContentTitle(title)
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText(msg))
+                        .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(Utils.BOSS_CARD_NOTIFICATION_ID, mBuilder.build());
     }
 }
