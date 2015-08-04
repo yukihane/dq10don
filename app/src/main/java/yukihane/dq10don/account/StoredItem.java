819e174a4de11c24cb5305e8e2bb4028e3ece058
yukihane <yukihane.feather@gmail.com>
2015-08-04 14:06:18 +0900

    残り時間を返すメソッドを追加

--- app/src/main/java/yukihane/dq10don/account/StoredItem.java
+++ app/src/main/java/yukihane/dq10don/account/StoredItem.java
@@ -4,6 +4,9 @@ package yukihane.dq10don.account;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import lombok.Getter;
 import lombok.Setter;
 import yukihane.dq10don.communication.dto.profile.ItemBasicListValueList;
@@ -13,6 +16,12 @@ import yukihane.dq10don.communication.dto.profile.ItemBasicListValueList;
  */
 @DatabaseTable
 public class StoredItem {
+
+
+    // あと192時間有効
+    // あと 52分 有効
+    // 「分」の場合は半角スペースが前後に入っている
+    private static final Pattern LEFT_TIME_PATTERN = Pattern.compile("あと ?(\\d+)([^ ]+) ?有効");
 
     @DatabaseField(id = true, canBeNull = false)
     private String itemUniqueNo;
@@ -51,5 +60,30 @@ public class StoredItem {
 
     public static StoredItem from(ItemBasicListValueList dto) {
         return new StoredItem(dto.getItemUniqueNo(), dto.getItemName(), dto.getWebItemId(), dto.getVariousStr());
+    }
+
+    /**
+     * 残り時間を返します. 単位は分です.
+     *
+     * @return 残り時間. 残り時間が設定されていないアイテムの場合はnull.
+     */
+    public Integer getLeftTime() {
+        if (variousStr == null) {
+            return null;
+        }
+        Matcher m = LEFT_TIME_PATTERN.matcher(variousStr);
+        if (m.matches()) {
+            String numStr = m.group(1);
+            String unit = m.group(2);
+            int num = Integer.parseInt(numStr);
+
+            // 「時間」意外の単位は「分」しか無いはず…
+            if ("時間".equals(unit)) {
+                return num * 60;
+            } else {
+                return num;
+            }
+        }
+        return null;
     }
 }
