c81d2c295ef6df6d73c270dca79585c3f81a0698
yukihane <yukihane.feather@gmail.com>
2015-08-04 14:18:34 +0900

    ボスカードサービス起動時設定

--- app/src/main/java/yukihane/dq10don/background/BossCardRestartReceiver.java
+++ app/src/main/java/yukihane/dq10don/background/BossCardRestartReceiver.java
@@ -5,47 +5,39 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
+
+import yukihane.dq10don.bosscard.view.BossCard;
+import yukihane.dq10don.db.BgServiceDao;
+import yukihane.dq10don.db.DbHelper;
+import yukihane.dq10don.db.DbHelperFactory;
+import yukihane.dq10don.settings.view.BossCardPrefUtils;
+import yukihane.dq10don.settings.view.TobatsuPrefUtils;
+
 /**
  * Created by yuki on 15/08/03.
  */
 public class BossCardRestartReceiver extends BroadcastReceiver {
-    /**
-     * This method is called when the BroadcastReceiver is receiving an Intent
-     * broadcast.  During this time you can use the other methods on
-     * BroadcastReceiver to view/modify the current result values.  This method
-     * is always called within the main thread of its process, unless you
-     * explicitly asked for it to be scheduled on a different thread using
-     * {@link Context#registerReceiver(BroadcastReceiver,
-     * IntentFilter, String, Handler)}. When it runs on the main
-     * thread you should
-     * never perform long-running operations in it (there is a timeout of
-     * 10 seconds that the system allows before considering the receiver to
-     * be blocked and a candidate to be killed). You cannot launch a popup dialog
-     * in your implementation of onReceive().
-     * <p>
-     * <p><b>If this BroadcastReceiver was launched through a &lt;receiver&gt; tag,
-     * then the object is no longer alive after returning from this
-     * function.</b>  This means you should not perform any operations that
-     * return a result to you asynchronously -- in particular, for interacting
-     * with services, you should use
-     * {@link Context#startService(Intent)} instead of
-     * {@link Context#bindService(Intent, ServiceConnection, int)}.  If you wish
-     * to interact with a service that is already running, you can use
-     * {@link #peekService}.
-     * <p>
-     * <p>The Intent filters used in {@link Context#registerReceiver}
-     * and in application manifests are <em>not</em> guaranteed to be exclusive. They
-     * are hints to the operating system about how to find suitable recipients. It is
-     * possible for senders to force delivery to specific recipients, bypassing filter
-     * resolution.  For this reason, {@link #onReceive(Context, Intent) onReceive()}
-     * implementations should respond only to known actions, ignoring any unexpected
-     * Intents that they may receive.
-     *
-     * @param context The Context in which the receiver is running.
-     * @param intent  The Intent being received.
-     */
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BossCardRestartReceiver.class);
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        LOGGER.info("onReceive called {}", intent.getAction());
 
+        BossCardPrefUtils prefUtils = new BossCardPrefUtils(context);
+        if (!prefUtils.isAutoPilotEnabled()) {
+            return;
+        }
+
+        String action = intent.getAction();
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
+            BossCardAlarm.set(context.getApplicationContext());
+        }
     }
 }
