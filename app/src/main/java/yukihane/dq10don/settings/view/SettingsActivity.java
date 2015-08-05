0dd1f397b4485967ce9da5021dac2e758c5f623d
yukihane <yukihane.feather@gmail.com>
2015-08-05 10:09:09 +0900

    設定のトップ画面もPreferenceActivityで実装する

--- app/src/main/java/yukihane/dq10don/settings/view/SettingsActivity.java
+++ app/src/main/java/yukihane/dq10don/settings/view/SettingsActivity.java
@@ -1,35 +1,16 @@
 package yukihane.dq10don.settings.view;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.preference.PreferenceActivity;
 
 import yukihane.dq10don.R;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
 
-        View tobatsu = findViewById(R.id.settingTobatsuView);
-        tobatsu.setOnClickListener((v) -> {
-            Intent intent = new Intent(SettingsActivity.this, TobatsuSettingActivity.class);
-            startActivity(intent);
-        });
-
-        View bossCard = findViewById(R.id.settingBossCardView);
-        bossCard.setOnClickListener((v) -> {
-            Intent intent = new Intent(SettingsActivity.this, BossCardSettingActivity.class);
-            startActivity(intent);
-        });
-
-        View twitter = findViewById(R.id.settingTwitterView);
-        twitter.setOnClickListener((v) -> {
-            Intent intent = new Intent(SettingsActivity.this, TwitterSettingActivity.class);
-            startActivity(intent);
-        });
+        addPreferencesFromResource(R.xml.pref_top);
     }
 }
