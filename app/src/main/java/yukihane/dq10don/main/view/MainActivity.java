38ca343bec8337b6888882e6c411b8b21eaf29bf
yukihane <yukihane.feather@gmail.com>
2015-08-06 17:35:02 +0900

    MainActivityのメニューは既存のものを使用する

--- app/src/main/java/yukihane/dq10don/main/view/MainActivity.java
+++ app/src/main/java/yukihane/dq10don/main/view/MainActivity.java
@@ -1,11 +1,15 @@
 package yukihane.dq10don.main.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import yukihane.dq10don.R;
+import yukihane.dq10don.debug.view.DebugActivity;
+import yukihane.dq10don.settings.view.SettingsActivity;
+import yukihane.dq10don.sqexid.view.SqexidActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,19 +22,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_base, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_sqexid) {
+            Intent intent = new Intent(this, SqexidActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_debug) {
+            Intent intent = new Intent(this, DebugActivity.class);
+            startActivity(intent);
             return true;
         }
 
