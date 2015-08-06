ccd01228e0ae1dbc06b7e952296aa17a028fafa9
yukihane <yukihane.feather@gmail.com>
2015-08-06 17:36:24 +0900

    メニューはMainActivityのみ

--- app/src/main/java/yukihane/dq10don/base/view/BaseActivity.java
+++ app/src/main/java/yukihane/dq10don/base/view/BaseActivity.java
@@ -58,34 +58,6 @@ public abstract class BaseActivity<P extends BaseActivityPresenter, A extends Ba
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_base, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_sqexid) {
-            Intent intent = new Intent(this, SqexidActivity.class);
-            startActivity(intent);
-            return true;
-        } else if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-            return true;
-        } else if (id == R.id.action_debug) {
-            Intent intent = new Intent(this, DebugActivity.class);
-            startActivity(intent);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     protected void onDestroy() {
         super.onDestroy();
         presenter.onDestroy();
