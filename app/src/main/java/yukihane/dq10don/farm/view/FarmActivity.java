96d88b738a2e159b293519051044f2d35fc58b92
yukihane <yukihane.feather@gmail.com>
2015-08-15 17:29:46 +0900

    牧場アクティビティ雛形

---
+++ app/src/main/java/yukihane/dq10don/farm/view/FarmActivity.java
@@ -0,0 +1,17 @@
+package yukihane.dq10don.farm.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import yukihane.dq10don.R;
+
+public class FarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farm);
+    }
+}
