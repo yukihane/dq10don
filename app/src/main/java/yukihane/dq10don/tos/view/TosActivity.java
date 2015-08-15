ffaf7f7d8a06a1687b4a7a6174bd6d950cca3977
yukihane <yukihane.feather@gmail.com>
2015-08-15 10:16:11 +0900

    利用規約アクティビティの雛形

---
+++ app/src/main/java/yukihane/dq10don/tos/view/TosActivity.java
@@ -0,0 +1,20 @@
+package yukihane.dq10don.tos.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import yukihane.dq10don.R;
+
+/**
+ * 利用規約(Terms of Service)を表示するアクティビティ.
+ */
+public class TosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tos);
+    }
+}
