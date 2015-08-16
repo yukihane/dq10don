cdefc1e39f69da7ddedbe241ad507dc1d482b36a
yukihane <yukihane.feather@gmail.com>
2015-08-16 21:54:55 +0900

    login DTO

---
+++ app/src/main/java/yukihane/dq10don/communication_game/dto/login/GameLoginDto.java
@@ -0,0 +1,79 @@
+
+package yukihane.dq10don.communication_game.dto.login;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("org.jsonschema2pojo")
+@JsonPropertyOrder({
+    "resultCode",
+    "data"
+})
+public class GameLoginDto {
+
+    @JsonProperty("resultCode")
+    private String resultCode;
+    @JsonProperty("data")
+    private Data data;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * 
+     * @return
+     *     The resultCode
+     */
+    @JsonProperty("resultCode")
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    /**
+     * 
+     * @param resultCode
+     *     The resultCode
+     */
+    @JsonProperty("resultCode")
+    public void setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    /**
+     * 
+     * @return
+     *     The data
+     */
+    @JsonProperty("data")
+    public Data getData() {
+        return data;
+    }
+
+    /**
+     * 
+     * @param data
+     *     The data
+     */
+    @JsonProperty("data")
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
