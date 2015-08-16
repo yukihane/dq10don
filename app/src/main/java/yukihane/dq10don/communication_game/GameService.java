a11625f6c9369b34aded7e7a59eccfab59251516
yukihane <yukihane.feather@gmail.com>
2015-08-16 21:55:37 +0900

    game service 雛形

---
+++ app/src/main/java/yukihane/dq10don/communication_game/GameService.java
@@ -0,0 +1,29 @@
+package yukihane.dq10don.communication_game;
+
+import retrofit.http.POST;
+import yukihane.dq10don.communication_game.dto.game.GameInfoDto;
+import yukihane.dq10don.communication_game.dto.game.MowGrassDto;
+import yukihane.dq10don.communication_game.dto.game.OpenAllTresureBoxDto;
+import yukihane.dq10don.communication_game.dto.game.ServerTimeDto;
+import yukihane.dq10don.communication_game.dto.login.GameLoginDto;
+
+/**
+ * Created by yuki on 2015/08/15.
+ */
+public interface GameService {
+
+    @POST("/member/login?data={force:true}&")
+    GameLoginDto login();
+
+    @POST("/farm/getinfo")
+    GameInfoDto getInfo();
+
+    @POST("/common/getservertime")
+    ServerTimeDto getServerTime();
+
+    @POST("/farm/openalltresurebox")
+    OpenAllTresureBoxDto openAllTresureBox();
+
+    @POST("/farm/mowgrass")
+    MowGrassDto mowGrass();
+}
---
+++ app/src/main/java/yukihane/dq10don/communication_game/dto/game/MowGrassDto.java
@@ -0,0 +1,7 @@
+package yukihane.dq10don.communication_game.dto.game;
+
+/**
+ * Created by yuki on 2015/08/15.
+ */
+public class MowGrassDto {
+}
