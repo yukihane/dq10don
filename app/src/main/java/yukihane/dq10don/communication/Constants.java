e2742d278c5d780a06056937433403eea91396bd
yukihane <yukihane.feather@gmail.com>
2015-08-21 14:44:01 +0900

    定数を定数クラスに外だし

---
+++ app/src/main/java/yukihane/dq10don/communication/Constants.java
@@ -0,0 +1,13 @@
+package yukihane.dq10don.communication;
+
+/**
+ * Created by yuki on 15/08/21.
+ */
+public final class Constants {
+    private Constants() {
+    }
+
+    public static final String HAPPYSERVICE_ENDPOINT = "https://happy.dqx.jp/capi";
+    public static final String HEADER_SESSION = "X-Smile-3DS-SESSIONID";
+
+}
