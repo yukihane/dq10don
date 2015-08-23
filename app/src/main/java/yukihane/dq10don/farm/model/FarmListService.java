b09af53b53e77f4a8adcdc5b4b4d7b6dcc569926
yukihane <yukihane.feather@gmail.com>
2015-08-24 07:13:24 +0900

    コメント編集

--- app/src/main/java/yukihane/dq10don/farm/model/FarmListService.java
+++ app/src/main/java/yukihane/dq10don/farm/model/FarmListService.java
@@ -4,7 +4,7 @@ import yukihane.dq10don.account.Farm;
 import yukihane.dq10don.base.model.BaseService;
 
 /**
- * Created by yuki on 2015/08/18.
+ * モンスター牧場情報を取得するサービス
  */
 public interface FarmListService extends BaseService<Farm> {
 }
