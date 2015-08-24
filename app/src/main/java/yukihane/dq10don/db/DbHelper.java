100498f78ce7e4a4ccf1fccbc66b36375c4f5bba
yukihane <yukihane.feather@gmail.com>
2015-08-25 07:42:35 +0900

    DBスキーマバージョン更新漏れ

--- app/src/main/java/yukihane/dq10don/db/DbHelper.java
+++ app/src/main/java/yukihane/dq10don/db/DbHelper.java
@@ -19,7 +19,7 @@ public class DbHelper extends OrmLiteSqliteOpenHelper {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DbHelper.class);
     private static final String DATABASE_NAME = "dq10don.db";
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
 
     public DbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
