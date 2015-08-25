e321c8670474fb8f083aef51249be8730d462e25
yukihane <yukihane.feather@gmail.com>
2015-08-25 14:03:15 +0900

    不要なthrowsを削除

--- app/src/main/java/yukihane/dq10don/communication_game/RetrofitGameService.java
+++ app/src/main/java/yukihane/dq10don/communication_game/RetrofitGameService.java
@@ -1,6 +1,5 @@
 package yukihane.dq10don.communication_game;
 
-import retrofit.http.Body;
 import retrofit.http.Field;
 import retrofit.http.FormUrlEncoded;
 import retrofit.http.POST;
@@ -9,7 +8,6 @@ import yukihane.dq10don.communication_game.dto.farm.mowgrass.MowGrassDto;
 import yukihane.dq10don.communication_game.dto.farm.openalltresurebox.OpenAllTresureBoxDto;
 import yukihane.dq10don.communication_game.dto.login.GameLoginDto;
 import yukihane.dq10don.communication_game.dto.time.ServerTimeDto;
-import yukihane.dq10don.exception.HappyServiceException;
 
 /**
  * Created by yuki on 2015/08/15.
@@ -18,22 +16,22 @@ public interface RetrofitGameService {
 
     @FormUrlEncoded
     @POST("/member/login")
-    GameLoginDto login(@Field("data") String data) throws HappyServiceException;
+    GameLoginDto login(@Field("data") String data);
 
     @FormUrlEncoded
     @POST("/farm/getinfo")
-    GameInfoDto getInfo(@Field("data") String data) throws HappyServiceException;
+    GameInfoDto getInfo(@Field("data") String data);
 
     @FormUrlEncoded
     @POST("/common/getservertime")
-    ServerTimeDto getServerTime(@Field("data") String data) throws HappyServiceException;
+    ServerTimeDto getServerTime(@Field("data") String data);
 
 
     @FormUrlEncoded
     @POST("/farm/mowgrass")
-    MowGrassDto mowGrass(@Field("data") String data) throws HappyServiceException;
+    MowGrassDto mowGrass(@Field("data") String data);
 
     @FormUrlEncoded
     @POST("/farm/openalltresurebox")
-    OpenAllTresureBoxDto openAllTresureBox(@Field("data") String data) throws HappyServiceException;
+    OpenAllTresureBoxDto openAllTresureBox(@Field("data") String data);
 }
